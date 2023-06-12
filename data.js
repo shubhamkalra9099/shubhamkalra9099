08ca15edee8a4c9bef5ea1f9072e5e331a09468b
shubhamkalra9099 <136348513+shubhamkalra9099@users.noreply.github.com>
2023-06-12 19:45:43 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://radical:vishu@radical.bvk17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5921174910:AAE_LzyPYPqOUHZmeQOeEe5CBSBo8Lm2Rvc',
+
+bot_name:'CNCAIRDROPWBOT',//bot username without @
+
+bot_admin:1811781327,
+
+channelsList : ['cncairdropbot'],
+
+channelscheck : ['@cncairdropbot'],
+
+airdropName : 'Cnc Airdrop.',
+
+reffer_bonus:300,// ral bonus amount
+
+min_wd:1000,//min withdrawal amount
+
+currency:'CNC', //bot cuurency
+
+payment_channel:'@cncpayout'// payment channel username
+
+}
